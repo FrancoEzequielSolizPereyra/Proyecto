b3f01e0ae733e8ae5eca4b4a77e63f952a186870
Franco Soliz <ezfrancosoliz23@gmail.com>
2022-09-01 19:26:49 -0300

    NavBar.js creado y simple (falta agregar estilos y decidir que mas agregarle)

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+
+const NavBar = () => {
+  return (
+    <div>
+      NavBar
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Favoritos</a></li>
+        <li><a href="#">Comunicate con nosotros</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
