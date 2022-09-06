849162a229a833044ce2e16325703f091e1507f8
Franco Soliz <ezfrancosoliz23@gmail.com>
2022-09-06 09:06:12 -0300

    iMenu sencillo E-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>E-commerce</h1>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Favoritos</a></li>
+        <li><a href="#">Comunicate con nosotros</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
